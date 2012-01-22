aafc0383a099cdd2863df9daab634f5827141919
rmerom@gmail.com <rmerom@gmail.com@696c9d85-52af-2463-33bc-b06103f29b57>
2012-01-22 14:44:28 +0000

    continue rename

--- src/com/yossale/client/actions/BucketExpenseServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package com.yossale.client.actions;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.yossale.client.data.ExpenseRecord;
-
-public interface BucketExpenseServiceAsync {
-	void getExpenses(long bucketId, AsyncCallback<ExpenseRecord[]> callback);
-}
