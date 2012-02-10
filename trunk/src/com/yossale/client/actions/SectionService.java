f1ea042bb5f98e6c37fc8500df5c54da90c79af0
yossale@gmail.com <yossale@gmail.com@696c9d85-52af-2463-33bc-b06103f29b57>
2012-02-10 22:51:52 +0000

    Search by section code

--- trunk/src/com/yossale/client/actions/SectionService.java
+++ trunk/src/com/yossale/client/actions/SectionService.java
@@ -7,13 +7,16 @@ import com.yossale.client.data.SectionRecord;
 @RemoteServiceRelativePath("section")
 public interface SectionService extends RemoteService {
 
-	SectionRecord[] getSections(int year);
-	void addSectionRecord(SectionRecord record);
+	SectionRecord[] getSections(int year);	
 	SectionRecord[] getSectionsByYear(int year);
 	SectionRecord[] getSectionsByYearAndParent(int year, String parentCode);
 	String[] getAvailableBudgetYears();
+	SectionRecord[] getSectionByYearAndCode(int year, String code);
+	SectionRecord[] getSectionsByNameAndCode(int year, String nameLike);
+	void addSectionRecord(SectionRecord record);
 	void removeAll();
 	void loadYearData(String year);
+	
 
 	
 }
