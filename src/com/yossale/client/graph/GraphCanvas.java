11666022606a531182fcf7efaa334a5bdc2bce5e
yossale@gmail.com <yossale@gmail.com@696c9d85-52af-2463-33bc-b06103f29b57>
2012-02-10 11:28:08 +0000

    Make the graph work again

--- src/com/yossale/client/graph/GraphCanvas.java
+++ src/com/yossale/client/graph/GraphCanvas.java
@@ -15,6 +15,7 @@ import com.google.gwt.visualization.client.VisualizationUtils;
 import com.google.gwt.visualization.client.visualizations.corechart.AreaChart;
 import com.google.gwt.visualization.client.visualizations.corechart.CoreChart;
 import com.google.gwt.visualization.client.visualizations.corechart.Options;
+import com.smartgwt.client.data.Record;
 import com.yossale.client.data.SectionRecord;
 
 public class GraphCanvas extends Composite {
@@ -22,16 +23,16 @@ public class GraphCanvas extends Composite {
   private AreaChart pie;
 
   /* The pie creation part should be in a different class * */
-  
+
   public GraphCanvas() {
-    
+
     final VerticalPanel widget = new VerticalPanel();
     initWidget(widget);
     widget.addStyleName("demo-Composite");
-    
+
     Runnable onLoadCallback = new Runnable() {
-      public void run() {        
-        pie = new AreaChart(createTable(null), createOptions());        
+      public void run() {
+        pie = new AreaChart(createTable(null), createOptions());
         widget.add(pie);
       }
     };
@@ -39,14 +40,13 @@ public class GraphCanvas extends Composite {
     VisualizationUtils.loadVisualizationApi(onLoadCallback, CoreChart.PACKAGE);
 
   }
-  
+
   public void updateGraph(List<SectionRecord> nodes) {
 
-    
-    if (nodes == null || nodes.isEmpty()) {    	
-    	return;    	
-    }      
-    
+    if (nodes == null || nodes.isEmpty()) {
+      return;
+    }
+
     pie.draw(createTable(nodes), createOptions());
   }
 
@@ -54,61 +54,59 @@ public class GraphCanvas extends Composite {
     Options options = Options.create();
     options.setWidth(600);
     options.setHeight(400);
-//     options.set3D(true);
-    options.setTitle("Government sections");    
+    // options.set3D(true);
+    options.setTitle("Government sections");
     return options;
   }
-  
+
   private List<SectionRecord> summarizeResults(List<SectionRecord> topics) {
-	  
-	  Map<Integer, SectionRecord> map = new HashMap<Integer, SectionRecord>();
-	  
-	  for(SectionRecord t : topics) {		  
-		  SectionRecord sum = map.get(t.getYear()) == null ? new SectionRecord() : map.get(t.getYear());		  
-		  sum.add(t);
-		  sum.setYear(t.getYear());
-		  map.put(t.getYear(), sum);		  
-	  }
-	  
-	  return new ArrayList<SectionRecord>(map.values());
-	  
+
+    Map<Integer, SectionRecord> map = new HashMap<Integer, SectionRecord>();
+
+    for (SectionRecord t : topics) {
+      SectionRecord sum = map.get(t.getYear()) == null ? new SectionRecord()
+          : map.get(t.getYear());
+      sum.add(t);
+      sum.setYear(t.getYear());
+      map.put(t.getYear(), sum);
+    }
+
+    return new ArrayList<SectionRecord>(map.values());
+
   }
 
   private DataTable createTable(List<SectionRecord> topics) {
-	  
-	System.out.println("Updating graph");
-	
-	
+
+    System.out.println("Updating graph");
+
     DataTable data = DataTable.create();
     data.addColumn(ColumnType.STRING, "Year");
     data.addColumn(ColumnType.NUMBER, "Net Allocated");
     data.addColumn(ColumnType.NUMBER, "Net Revised");
-    data.addColumn(ColumnType.NUMBER, "Net Used");   
-    
+    data.addColumn(ColumnType.NUMBER, "Net Used");
+
     if (topics == null || topics.isEmpty()) {
       return data;
     }
-    
+
     List<SectionRecord> sums = summarizeResults(topics);
     Collections.sort(sums, new Comparator<SectionRecord>() {
 
       @Override
-      public int compare(SectionRecord o1, SectionRecord o2) {        
-        return (new Integer(o1.getYear()).compareTo(new Integer(o2.getYear()))  );
+      public int compare(SectionRecord o1, SectionRecord o2) {
+        return (new Integer(o1.getYear()).compareTo(new Integer(o2.getYear())));
       }
     });
 
     for (SectionRecord t : sums) {
       int rowIndex = data.addRow();
-      data.setValue(rowIndex, 0, t.getYear()+"");
+      data.setValue(rowIndex, 0, t.getYear() + "");
       data.setValue(rowIndex, 1, t.getNetAmountAllocated());
       data.setValue(rowIndex, 2, t.getNetAmountRevised());
       data.setValue(rowIndex, 3, t.getNetAmountUsed());
     }
-    
+
     return data;
   }
-
-
 
 }
