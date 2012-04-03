e0ccdb52ebccb1581edbd6c4f3e8321a3e6b78fb
rmerom@gmail.com <rmerom@gmail.com@696c9d85-52af-2463-33bc-b06103f29b57>
2012-04-03 06:58:32 +0000

    delete all GWT client code.

--- trunk/src/com/yossale/client/actions/LoginServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package com.yossale.client.actions;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.yossale.client.data.LoginInfo;
-
-public interface LoginServiceAsync {
-  public void login(String requestUri, AsyncCallback<LoginInfo> async);
-}
