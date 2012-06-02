a7b61d6026adccabead57317c61e50e1d794357b
yossale@gmail.com <yossale@gmail.com@696c9d85-52af-2463-33bc-b06103f29b57>
2012-06-02 09:56:17 +0000

    Add the graphs

--- war/new/js/presentation.js
+++ war/new/js/presentation.js
@@ -49,13 +49,14 @@ $(document).ready(function() {
 	});
 	// Register an event on the add button retrieve the given code.
 	$('#add_expense').click(function() {
-	  var code = ""+$('#expense_code').val();
-    $('#expense_code').val('');  // Clear textbox of old value.
-    var expense = {
-      code: code,
-      weight: parseInt($('#expense_weight').val()) / 100.0
-    };
-	  getExpenses([expense], getExpenseInfoCallback);
+		var code = ""+$('#expense_code').val();
+		$('#expense_code').val('');  // Clear textbox of old value.
+		var expense = {
+		  code: code,
+		  weight: parseInt($('#expense_weight').val()) / 100.0
+		};
+		  getExpenses([expense], getExpenseInfoCallback);
+		  generateGraph('expenses_graph')
 	});
 	var thisYear = new Date().getFullYear();
   var select = $('#yearsSelect');
@@ -124,36 +125,48 @@ function refreshUI() {
   // Refresh the table.
   // Sum over the years.
   var sums = {};
+  var sumsByYear = {};
+  
   for (code in expensesByCodeThenYear) {
     var expense = expensesByCodeThenYear[code];
     var differentTitles = false;
     for (year in expense.years) {
-      if (-1 == $.inArray(year, $('#yearsSelect').val())) {
-        continue; 
-      }
-      var item = expense.years[year];
-	    if (!sums[code]) {
-	      sums[code] = { 
-	        code: item.code, 
-          weight: expense.weight,
-	        title: item.title, 
-	        net_allocated: 0, 
-	        net_revised: 0, 
-	        net_used: 0,
-	        gross_revised: 0,
-	        gross_used: 0 };
-	    } else {
-        if (sums[code].title != item.title && $.inArray(code, expensesWarned) == -1) {
-          expensesWarned.push(code);
-          prettyAlert('שימו לב: סעיף ' + code + ' מכיל לאורך השנים תיאורים שונים.</br>' +
-              'בשלב זה חוקר התקציב אינו מאפשר הפרדה אוטומטית בין שנים אלה. אנא בידקו בתקציב הפתוח אילו שנים רלוונטיות עבורכם.');
-        }
-      }
+		if (-1 == $.inArray(year, $('#yearsSelect').val())) {
+			continue; 
+		}
+		var item = expense.years[year]; 
+	  
+		if (!sums[code]) {
+		  sums[code] = { 
+			code: item.code, 
+			weight: expense.weight,
+			title: item.title, 
+			net_allocated: 0, 
+			net_revised: 0, 
+			net_used: 0,
+			gross_revised: 0,
+			gross_used: 0 };
+		} else {
+		  if (sums[code].title != item.title && $.inArray(code, expensesWarned) == -1) {
+			expensesWarned.push(code);
+			prettyAlert('שימו לב: סעיף ' + code + ' מכיל לאורך השנים תיאורים שונים.</br>' +
+			  'בשלב זה חוקר התקציב אינו מאפשר הפרדה אוטומטית בין שנים אלה. אנא בידקו בתקציב הפתוח אילו שנים רלוונטיות עבורכם.');
+			}
+		}
+		
 	    item.net_allocated = parseInt(item.net_allocated);
 	    item.net_revisited = parseInt(item.net_revisited);
 	    item.net_used = parseInt(item.net_used);
 	    item.gross_revised = parseInt(item.gross_revised);
-	    var currentSums = sums[code];
+	    
+		//Yossi: compute the expenses by year
+		if (!sumsByYear[year]) {
+        	sumsByYear[year] = 0;
+        }
+        sumsByYear[year] += expense.weight * nanZero(item.net_allocated)       
+		//Yossi - end section
+		
+		var currentSums = sums[code];
 	    currentSums.net_allocated += nanZero(item.net_allocated);
 	    currentSums.net_revised += nanZero(item.net_revised);
 	    currentSums.net_used += nanZero(item.net_used);
@@ -161,18 +174,18 @@ function refreshUI() {
 	    currentSums.gross_used += nanZero(item.gross_used);
 	    
 	    var row = [];
-  	  row.push(item.code);
-  	  row.push(item.title);
-  	  row.push(year);
-      row.push(Math.round(expense.weight * 100));
-  	  row.push(parseInt(item.net_allocated * expense.weight));
-  	  row.push(parseInt(item.net_revised * expense.weight));
-  	  row.push(parseInt(item.gross_revised * expense.weight));
-  	  row.push(parseInt(item.net_used * expense.weight));
-  	  row.push(parseInt(item.gross_used * expense.weight));
-      row.push('<a href="javascript:deleteExpense(\'' + item.code + '\')">מחקו</a>');
-      oTable.fnAddData(row);
-    
+		row.push(item.code);
+		row.push(item.title);
+		row.push(year);
+		row.push(Math.round(expense.weight * 100));
+		row.push(parseInt(item.net_allocated * expense.weight));
+		row.push(parseInt(item.net_revised * expense.weight));
+		row.push(parseInt(item.gross_revised * expense.weight));
+		row.push(parseInt(item.net_used * expense.weight));
+		row.push(parseInt(item.gross_used * expense.weight));
+		row.push('<a href="javascript:deleteExpense(\'' + item.code + '\')">מחקו</a>');
+		oTable.fnAddData(row);
+      
     }
   }
   // Now add the sums to the table.
@@ -183,25 +196,27 @@ function refreshUI() {
     var row = [];
 	  row.push(item.code);
 	  $("#group-id-output_table-"+item.code+" td").attr("colspan",1);
-    row.push(item.title);
+	row.push(item.title);
 	  groupTr.append($("<td>").addClass(tdClass).text(item.title));
 	  groupTr.append($("<td>").addClass(tdClass).text(""));
-    row.push(Math.round(item.weight * 100));
-    groupTr.append($("<td>").addClass(tdClass).text(Math.round(item.weight * 100)));
-    row.push(parseInt(item.net_allocated * item.weight));
+	row.push(Math.round(item.weight * 100));
+	groupTr.append($("<td>").addClass(tdClass).text(Math.round(item.weight * 100)));
+	row.push(parseInt(item.net_allocated * item.weight));
 	  groupTr.append($("<td>").addClass(tdClass).text(parseInt(item.net_allocated * item.weight)));
-    row.push(parseInt(item.net_revised * item.weight));
+	row.push(parseInt(item.net_revised * item.weight));
 	  groupTr.append($("<td>").addClass(tdClass).text(parseInt(item.net_revised * item.weight)));
-    row.push(parseInt(item.gross_revised * item.weight));
+	row.push(parseInt(item.gross_revised * item.weight));
 	  groupTr.append($("<td>").addClass(tdClass).text(parseInt(item.gross_revised * item.weight)));
 	  row.push(parseInt(item.net_used * item.weight));
 	  groupTr.append($("<td>").addClass(tdClass).text(parseInt(item.net_used * item.weight)));
-    row.push(parseInt(item.gross_used * item.weight));
+	row.push(parseInt(item.gross_used * item.weight));
 	  groupTr.append($("<td>").addClass(tdClass).text(parseInt(item.gross_used * item.weight)));
-    row.push('<a href="javascript:deleteExpense(\'' + item.code + '\')">מחקו</a>');
-    groupTr.append($("<td>").addClass(tdClass).text(""));
-    //oTable.fnAddData(row);
+	row.push('<a href="javascript:deleteExpense(\'' + item.code + '\')">מחקו</a>');
+	groupTr.append($("<td>").addClass(tdClass).text(""));
+	// oTable.fnAddData(row);
   });
+  generateGraph('expenses_graph', sumsByYear);
+  
 }
 
 function aRequest(isStart) {
@@ -241,6 +256,70 @@ function prettyAlert(msg) {
 	$("#msgBox").empty();
 	$("#msgBox").append(msg);
 	$("#msgBox").dialog('open');
+}
+
+function generateGraph(containerName, sumsByYearMap) {
+	
+	var xCategories = Object.keys(sumsByYearMap).sort()
+	//Currently I'm only using 1 series - net allocated. but later I'll have to add some more
+	var netValueSeries = jQuery.map(xCategories, function(n) {
+      return (sumsByYearMap[n]);
+    });
+	
+	var graphInfo = { 
+			name: 'NetValue',
+	        data: netValueSeries
+	}
+	 
+	chart = new Highcharts.Chart({
+            chart: {
+                renderTo: containerName,
+                type: 'area'
+            },
+            title: {
+                text: 'ניצול סעיפים לפי שנים'
+            },
+            subtitle: {
+                text: 'כותרת משנה'
+            },
+            xAxis: {
+                categories: xCategories,
+                tickmarkPlacement: 'on',
+                title: {
+                    // enabled: false
+					text: 'שנים'
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'שח'
+                },
+                labels: {
+//                    formatter: function() {
+//                        return this.value / 1000;
+//                    }
+                }
+            },
+            tooltip: {
+                formatter: function() {
+                    return ''+
+                        this.x +': '+ Highcharts.numberFormat(this.y, 0, ',') +' millions';
+                }
+            },
+            plotOptions: {
+                area: {
+                    stacking: 'normal',
+                    lineColor: '#666666',
+                    lineWidth: 1,
+                    marker: {
+                        lineWidth: 1,
+                        lineColor: '#666666'
+                    }
+                }
+            },
+            series: [graphInfo]
+        });
+
 }
 
 function prettyConfirm(msg, callbackYes, callbackNo) {
